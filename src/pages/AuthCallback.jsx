272d0d5fe79acb0cfe5edd4de8caa41f7f1d2be3
BJ Toy <bjtoy69@gmail.com>
2026-05-30 12:58:48 +0930

    Fix auth callback double auth request

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -1,79 +1,28 @@
-import {
-  useEffect,
-} from "react";
-
-import {
-  useNavigate,
-} from "react-router-dom";
-
-import {
-  useRoles,
-} from "../context/RoleContext.jsx";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRoles } from "../context/RoleContext.jsx";
 
 export default function AuthCallback() {
-
-  const navigate =
-    useNavigate();
+  const navigate = useNavigate();
 
   const {
     user,
     loading,
-    refreshUser,
   } = useRoles();
 
-  /**
-   * =========================
-   * HYDRATE AUTH
-   * =========================
-   */
   useEffect(() => {
+    if (loading) return;
 
-    refreshUser();
-
-  }, []);
-
-  /**
-   * =========================
-   * WAIT FOR AUTH
-   * =========================
-   */
-  useEffect(() => {
-
-    /**
-     * STILL LOADING
-     */
-    if (loading) {
-      return;
-    }
-
-    /**
-     * AUTHENTICATED
-     */
     if (user) {
-
-      navigate(
-        "/member",
-        {
-          replace: true,
-        }
-      );
-
+      navigate("/member", {
+        replace: true,
+      });
       return;
     }
 
-    /**
-     * FAILED LOGIN
-     */
-    if (user === null) {
-
-      navigate(
-        "/login",
-        {
-          replace: true,
-        }
-      );
-    }
-
+    navigate("/login", {
+      replace: true,
+    });
   }, [
     user,
     loading,
